cfeb072a80fef0e0b8033166c6a4aea9ae59de12
kbpsystem <koleen.bp@outlook.com>
2017-11-23 04:08:15 +0800

    added iframe video

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
+class Player extends React.Component {
+  render() {
+    return (
+      <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/4Q46xYqUwZQ?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+      </div>
+    );
+
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +28,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <Player />
+        </div>
       </div>
     );
   }
