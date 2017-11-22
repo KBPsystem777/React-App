e1fcace80ba2fe22f590e19e51e66d6b4494b3d0
kbpsystem <koleen.bp@outlook.com>
2017-11-23 05:28:04 +0800

    dev-stage-1 commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -10,25 +9,29 @@ class Player extends React.Component {
   render() {
     return (
       <div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/4Q46xYqUwZQ?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+        <iframe className="img-responsive text-center" title="Legends Never Die" src="https://www.youtube.com/embed/4Q46xYqUwZQ?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
       </div>
     );
 
   }
 }
 
+class NavBar extends React.Component {
+  render () {
+    return (
+      <div>
+        <nav className="nav"></nav>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
         <div>
+          <NavBar />
           <Player />
         </div>
       </div>
